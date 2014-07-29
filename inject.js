179541252ea131794c014a86a7da563ef891286b
Robin Moisson <robin.moisson@gmail.com>
2014-07-29 20:13:40 +0300

    Initial commit, working plugin

---
+++ inject.js
@@ -0,0 +1,51 @@
+// This helps avoid conflicts in case we inject
+// this script on the same page multiple times
+// without reloading.
+var clean_copy = clean_copy || (function(){
+
+  var methods = {};
+
+  methods.kopy =function(){
+    var tabUrl = document.URL;
+    if (tabUrl.match(/^https?:\/\/www.facebook.com/) === null) return false;
+
+    var id;
+    id = tabUrl.match(/\/(\d+)\//);
+    if (id !== null && id[1] !== null) return id[1];
+    id = tabUrl.match(/fbid=(\d+)/);
+    if (id !== null && id[1] !== null) return id[1];
+    id = tabUrl.match(/\Wid=(\d+)/);
+    if (id !== null && id[1] !== null) return id[1];
+
+    var graphUrl = tabUrl.replace('http://www', 'http://graph');
+    graphUrl = tabUrl.replace('https://www', 'https://graph');
+    $.ajax({
+      type: "GET",
+      url: graphUrl,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      cache: true,
+      async: false
+    }).done(function(data) {
+      id = data.id;
+    });
+    if (id !== null && id !== "") return id;
+    return false;
+  };
+
+  // This tells the script to listen for
+  // messages from our extension.
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    var data = {};
+    // If the method the extension has requested
+    // exists, call it and assign its response
+    // to data.
+    if (methods.hasOwnProperty(request.method))
+      data = methods[request.method]();
+    // Send the response back to our extension.
+    sendResponse({ data: data });
+    return true;
+  });
+
+  return true;
+})();
---
+++ popup.js
@@ -0,0 +1,51 @@
+jQuery.fn.selectText = function(){
+    var doc = document,
+        element = this[0],
+        range,
+        selection;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
+
+var cleanKopy = {
+  injectedMethod: function (tab, method, callback) {
+    chrome.tabs.executeScript(tab.id, { file: "jquery.js" }, function() {
+      chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
+        chrome.tabs.sendMessage(tab.id, { method: method }, callback);
+      });
+    });
+  },
+
+  kopy: function (tab) {
+    this.injectedMethod(tab, 'kopy', function(response){
+      if (typeof response.data === 'undefined' || response.data === false){
+        return window.close();
+      }
+      $('#clean-link').text("https://www.facebook.com/" + response.data);
+      $('#clean-link').selectText();
+      return true;
+    });
+  }
+};
+
+function KeyPress(e) {
+  var evtobj = window.event ? event : e;
+  if (evtobj.keyCode == 67 && evtobj.ctrlKey) window.close();
+}
+document.onkeydown = KeyPress;
+
+chrome.tabs.query(
+  { active: true, currentWindow: true },
+  function (tabs) {
+    cleanKopy.kopy(tabs[0]);
+  }
+);
